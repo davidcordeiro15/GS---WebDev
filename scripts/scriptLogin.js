65619d02ac92f76a1443b799930d17ccab9d8b97
David_Cordeiro <160193115+davidcordeiro15@users.noreply.github.com>
2024-06-03 22:00:48 -0300

    Criando os inputs para denuncia

--- scripts/scriptLogin.js
+++ scripts/scriptLogin.js
@@ -2,6 +2,7 @@ const inputCpf = document.querySelector('#loginCpf')
 const inputNome = document.querySelector('#loginNome')
 const inputEmail = document.querySelector('#loginEmail')
 const inputCheck = document.querySelector('#loginCheck')
+//const inputData = document.querySelector('#loginData')
 
 function validaNumero(valor, msg) {
     let input = valor
@@ -21,15 +22,28 @@ function validaEspaços(espaco, msg) {
     }   
 }
 
+function validaEmail(email, msg) {
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    )
+    if (emailRegex.test(email)) {
+        return true
+    } else {
+        return alert(msg)
+    }
+}
+
+
 function criaConta() {
-    console.log(inputCpf.value)
+    console.log(inputData.value)
     let a = validaNumero(inputCpf.value, "Digite novamente o Cpf")
     let b = validaEspaços(inputNome.value, "Digite novamente seu nome")
-    let c = validaEspaços(inputEmail.value, "Digite novamente seu email")
+    let c = validaEmail(inputEmail.value, "Digite novamente seu email")
+    //let d = validaNumero(inputData.value, "Digite seu ano de nascimento novamente")
     if (a === true & b === true & c === true & inputCheck.checked) {
         alert("Conta criada com sucesso! ")
         location.reload()   
     } else {
-        alert("Sua conta não foi criada, confirme o checklist para criar")
+        alert("Sua conta não foi criada, verifique se todos os campos estão preenchidos corretamente!")
     }
 }
