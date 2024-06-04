5024f36bbad860eceba6409521874f7052ba72f1
David_Cordeiro <160193115+davidcordeiro15@users.noreply.github.com>
2024-06-04 10:20:37 -0300

    Criando a pagina de cadastro e suas validações

--- scripts/scriptLogin.js
+++ scripts/scriptLogin.js
@@ -2,14 +2,27 @@ const inputCpf = document.querySelector('#loginCpf')
 const inputNome = document.querySelector('#loginNome')
 const inputEmail = document.querySelector('#loginEmail')
 const inputCheck = document.querySelector('#loginCheck')
-//const inputData = document.querySelector('#loginData')
+const inputSenha = document.querySelector('#loginSenha')
+const listaCadastro = document.querySelector('#listaCadastro')
+const btnCriar = document.querySelector('#btnCriar')
+var contas = [
+    {
+        nome: 'David',
+        cpf: 1234,
+        email: 'david@gmail.com',
+        senha: 'david',
+    }
+]
+
+
 
 function validaNumero(valor, msg) {
     let input = valor
     if (isNaN(input) || input === '') {
         alert(msg)
+        return false
     } else {
-        return true
+        return valor
     }
     
 }
@@ -17,8 +30,9 @@ function validaEspaços(espaco, msg) {
     let input = espaco
     if (input === '') {
         alert(msg)
+        return false
     } else {
-        return true
+        return espaco
     }   
 }
 
@@ -27,23 +41,60 @@ function validaEmail(email, msg) {
         /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
     )
     if (emailRegex.test(email)) {
-        return true
+        return email
     } else {
-        return alert(msg)
+        alert(msg)
+        return false
     }
+}
+function adicionaNaConta(nome, cpf, email, senha) {
+    let conta = [
+        {
+            nome: nome,
+            cpf: cpf,
+            email: email,
+            senha: senha,
+        }
+    ]
+    contas.unshift(conta)
+    //listaCadastro.innerHTML = ''
 }
 
 
+
+
+
 function criaConta() {
-    console.log(inputData.value)
-    let a = validaNumero(inputCpf.value, "Digite novamente o Cpf")
-    let b = validaEspaços(inputNome.value, "Digite novamente seu nome")
-    let c = validaEmail(inputEmail.value, "Digite novamente seu email")
+    
+    let cpfValor = validaNumero(inputCpf.value, "Digite novamente o Cpf")
+    let nomeValor = validaEspaços(inputNome.value, "Digite novamente seu nome")
+    let emailValor = validaEmail(inputEmail.value, "Digite novamente seu email")
+    let senhaValor = validaEspaços(inputSenha.value, "Digite sua senha novamente")
     //let d = validaNumero(inputData.value, "Digite seu ano de nascimento novamente")
-    if (a === true & b === true & c === true & inputCheck.checked) {
+    
+    if (cpfValor !== false & nomeValor !== false & emailValor !== false & senhaValor !== false & inputCheck.checked) {
+        
+        adicionaNaConta(nomeValor, cpfValor, emailValor, senhaValor)
+        
+        console.log(contas)
         alert("Conta criada com sucesso! ")
-        location.reload()   
+        cpfValor.innerHTML = ''
+        nomeValor.innerHTML = ''
+        emailValor.innerHTML = ''
+        senhaValor.innerHTML = ''
+           
     } else {
         alert("Sua conta não foi criada, verifique se todos os campos estão preenchidos corretamente!")
     }
 }
+
+btnCriar.addEventListener('click', function(infosDoEvento) {
+    infosDoEvento.preventDefault()
+
+    criaConta()
+    
+        
+
+})
+
+console.log(contas)
