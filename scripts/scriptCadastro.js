666b9399092ced2d689a7582be0660eaf7304c63
David_Cordeiro <160193115+davidcordeiro15@users.noreply.github.com>
2024-06-06 15:04:56 -0300

    Atualizar o scriptCadastro.js

--- scripts/scriptCadastro.js
+++ scripts/scriptCadastro.js
@@ -139,7 +139,7 @@ btnLogin.addEventListener('click', function(logar) {
             alert('Login realizado!');
 
             //entrar na pagina de denuncia após logar 
-            window.location = "pages/denuncia.html";
+            window.location = "../pages/denuncia.html";
             break;
         } else {
             i++;
